cef30afeb487d8f2658fc8dec637b1f06abae7a2
Aashish909 <72991367+Aashish909@users.noreply.github.com>
2023-08-27 12:00:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import cors from "cors";
 
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
+import  {fileURLToPath } from 'url';
 
 //config env
 dotenv.config();
 
 //database comfig
 connectDB();
+
+//ES Module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
